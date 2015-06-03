26a4b53fd3878729908a6cc3f3aa4530a9a076bb
sporeventexplosion <sporeventexplosion@gmail.com>
2015-06-03 20:38:29 +0300

    Add Rittybox library autoloading

--- src/index.php
+++ src/index.php
@@ -2,10 +2,20 @@
 
 require_once '../vendor/autoload.php';
 
+require_once './lib/Rittybox/Autoloader.php';
+
+
+//Register the autoloader
+
+$Autoloader = new Rittybox\Autoloader();
+$Autoloader::register();
+
 $loader = new Twig_Loader_Filesystem('./template');
 $twig = new Twig_Environment($loader, array(
     //'cache' => './twig-compilation-cache',
 ));
+
+
 
 echo $twig->render('index.html', array('name' => 'Test'));
 
