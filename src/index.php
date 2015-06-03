e11e4eda81ca9bcb569f5cd319217e88d0031740
sporeventexplosion <sporeventexplosion@gmail.com>
2015-06-03 20:06:08 +0300

    Disable Twig cache

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@ require_once '../vendor/autoload.php';
 
 $loader = new Twig_Loader_Filesystem('./template');
 $twig = new Twig_Environment($loader, array(
-    'cache' => './twig-compilation-cache',
+    //'cache' => './twig-compilation-cache',
 ));
 
 echo $twig->render('index.html', array('name' => 'Test'));
