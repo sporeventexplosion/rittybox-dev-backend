c86f56e94aec790bfde6c25a6b58819392a2fae5
sporeventexplosion <sporeventexplosion@gmail.com>
2015-06-04 16:15:14 +0300

    Add basic utilities

---
+++ src/lib/Rittybox/util.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Rittybox;
+
+class Util {
+  public static function get_random_hash() {
+    $length = 64;
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[mt_rand(0, $charactersLength - 1)];
+    }
+    return hash('sha256', $randomString);
+  }
+}
