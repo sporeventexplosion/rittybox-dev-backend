a0fabc82a40413705982f55297416954b6bbb069
sporeventexplosion <sporeventexplosion@gmail.com>
2015-06-04 19:10:03 +0300

    Add basic Database class to facilitate easier database usage

--- src/lib/Rittybox/Autoloader.php
+++ src/lib/Rittybox/Autoloader.php
@@ -4,30 +4,34 @@ namespace Rittybox;
 
 class Autoloader
 {
-    private $dir;
-    public function __construct($dir = null)
-    {
-        if (is_null($dir)) {
-            $dir = dirname(__FILE__).'/..';
-        }
-        $this->dir = $dir;
+  private $dir;
+  public function __construct($dir = null)
+  {
+    if (is_null($dir)) {
+      $dir = dirname(__FILE__).'/..';
     }
+    $this->dir = $dir;
+  }
 
 
-    public static function register($dir = null)
-    {
-        ini_set('unserialize_callback_func', 'spl_autoload_call');
-        spl_autoload_register(array(new self($dir), 'autoload'));
+  public static function register($dir = null)
+  {
+    ini_set('unserialize_callback_func', 'spl_autoload_call');
+    spl_autoload_register(array(new self($dir), 'autoload'));
+  }
+
+
+  public function autoload($class)
+  {
+    if (0 !== strpos($class, 'Rittybox')) {
+      return;
     }
-
-
-    public function autoload($class)
-    {
-        if (0 !== strpos($class, 'Rittybox')) {
-            return;
-        }
-        if (file_exists($file = $this->dir.'/'.str_replace('\\', '/', $class).'.php')) {
-            require $file;
-        }
+    if (file_exists($file = $this->dir.'/'.str_replace('\\', '/', $class).'.php')) {
+      require $file;
     }
+  }
+
+  public static function setDatabase ($database) {
+    $Database = $database;
+  }
 }
